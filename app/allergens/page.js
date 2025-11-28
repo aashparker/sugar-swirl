f0dc7785f7555598a1a37e16a126caa3378bd861
aashparker <aashiqp995@gmail.com>
2025-11-29 00:21:55 +0200

    final clean up

--- app/allergens/page.js
+++
@@ -1,9 +0,0 @@
-export default function Allergens() {
-    return (
-        <>
-            <div className='flex justify-center items-center'>
-                Hello Allergens.
-            </div>
-        </>
-    )
-}
